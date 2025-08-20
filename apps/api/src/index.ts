40ccfece9c2c6082e4c6162e9d77ca92b671d50f
Akdev Saha <akdevsaha2003@gmail.com>
2025-08-20 21:33:17 +0530

    feat: setup ngrok tunnel and initialize database

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,1 +1,26 @@
-console.log('Hello from API!');
+import express from "express";
+import ngrok from "@ngrok/ngrok";
+import { prisma } from "@repo/db"
+const app = express();
+const port = 3000;
+
+app.get("/", async (_req, res) => {
+    const user = await prisma.user.findFirst()
+    res.json({ message: "Hello World", user: user?.name ?? "No user found" });
+});
+
+app.listen(port, async () => {
+    console.log(`Server running on http://localhost:${port}`);
+
+    try {
+        const listener = await ngrok.connect({
+            addr: port,
+            authtoken: process.env.NGROK_AUTHTOKEN, // ✅ explicit token
+            domain: "ghoul-ready-moccasin.ngrok-free.app"
+        });
+
+        console.log(`Ingress established at: ${listener.url()}`);
+    } catch (err) {
+        console.error("Ngrok failed:", err);
+    }
+});
