5c423579b32230d3dc5fe3d739cd53d1f9a0d8ca
Akdev Saha <akdevsaha2003@gmail.com>
2025-08-24 23:52:16 +0530

    ui

---
+++ packages/ui/src/components/dropdown-menu.tsx
@@ -0,0 +1,127 @@
+"use client"
+
+import * as React from "react"
+import { clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+
+function cn(...inputs: any[]) {
+  return twMerge(clsx(inputs))
+}
+
+interface DropdownMenuProps {
+  children: React.ReactNode
+}
+
+interface DropdownMenuTriggerProps {
+  asChild?: boolean
+  children: React.ReactNode
+}
+
+interface DropdownMenuContentProps {
+  align?: "start" | "center" | "end"
+  children: React.ReactNode
+}
+
+interface DropdownMenuItemProps {
+  onClick?: () => void
+  children: React.ReactNode
+}
+
+const DropdownMenuContext = React.createContext<{
+  isOpen: boolean
+  setIsOpen: (open: boolean) => void
+}>({
+  isOpen: false,
+  setIsOpen: () => {},
+})
+
+export function DropdownMenu({ children }: DropdownMenuProps) {
+  const [isOpen, setIsOpen] = React.useState(false)
+
+  return (
+    <DropdownMenuContext.Provider value={{ isOpen, setIsOpen }}>
+      <div className="relative inline-block text-left">
+        {children}
+      </div>
+    </DropdownMenuContext.Provider>
+  )
+}
+
+export function DropdownMenuTrigger({ asChild, children }: DropdownMenuTriggerProps) {
+  const { isOpen, setIsOpen } = React.useContext(DropdownMenuContext)
+
+  const handleClick = () => {
+    setIsOpen(!isOpen)
+  }
+
+  if (asChild && React.isValidElement(children)) {
+    return React.cloneElement(children, {
+      onClick: handleClick,
+    } as any)
+  }
+
+  return (
+    <button onClick={handleClick}>
+      {children}
+    </button>
+  )
+}
+
+export function DropdownMenuContent({ align = "center", children }: DropdownMenuContentProps) {
+  const { isOpen, setIsOpen } = React.useContext(DropdownMenuContext)
+  const contentRef = React.useRef<HTMLDivElement>(null)
+
+  React.useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (contentRef.current && !contentRef.current.contains(event.target as Node)) {
+        setIsOpen(false)
+      }
+    }
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside)
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [isOpen, setIsOpen])
+
+  if (!isOpen) return null
+
+  const alignmentClasses = {
+    start: "left-0",
+    center: "left-1/2 -translate-x-1/2",
+    end: "right-0",
+  }
+
+  return (
+    <div
+      ref={contentRef}
+      className={cn(
+        "absolute z-50 mt-2 w-56 rounded-md border border-neutral-200 bg-white py-1 shadow-lg dark:border-neutral-800 dark:bg-neutral-950",
+        alignmentClasses[align]
+      )}
+    >
+      {children}
+    </div>
+  )
+}
+
+export function DropdownMenuItem({ onClick, children }: DropdownMenuItemProps) {
+  const { setIsOpen } = React.useContext(DropdownMenuContext)
+
+  const handleClick = () => {
+    onClick?.()
+    setIsOpen(false)
+  }
+
+  return (
+    <button
+      className="block w-full px-4 py-2 text-left text-sm text-neutral-700 hover:bg-neutral-100 dark:text-neutral-300 dark:hover:bg-neutral-800"
+      onClick={handleClick}
+    >
+      {children}
+    </button>
+  )
+}
--- packages/ui/src/components/inputBox.tsx
+++ packages/ui/src/components/inputBox.tsx
@@ -1,20 +1,20 @@
 interface inputBoxProps {
     label: string,
     placeholder: string,
-    value?: React.InputHTMLAttributes<HTMLInputElement>,
-    onChange?: React.ChangeEventHandler<HTMLInputElement>,
-    onKeyDown?: React.KeyboardEventHandler<HTMLInputElement>,
-    type?:React.InputHTMLAttributes<HTMLInputElement>,
+    value: string
+    onChange: React.ChangeEventHandler<HTMLInputElement>,
+    onKeyDown: React.KeyboardEventHandler<HTMLInputElement>,
+    type: React.HTMLInputTypeAttribute,
     ref?: React.Ref<HTMLInputElement>
 }
 
-export const InputBox = ({label, placeholder, value, onChange,onKeyDown, type, ref}: inputBoxProps) =>{
-        return <div className="w-full">
-            <div className="text-[white]/90 mt-4">
-                {label}
-            </div>
-            <input className="text-neutral-600 focus:bg-neutral-900 w-[400px] rounded-md mt-3 px-3 py-2 focus:outline-none border-[1px] border-neutral-800 focus:border-[1px] focus:border-neutral-300" placeholder={placeholder} />
+export const InputBox = ({ label, placeholder, value, onChange, onKeyDown, type, ref }: inputBoxProps) => {
+    return <div className="w-full">
+        <div className="dark:text-[white]/90 text-black mt-4">
+            {label}
         </div>
+        <input ref={ref} type={type} value={value} onKeyDown={onKeyDown} onChange={onChange} className="text-neutral-500 dark:text-neutral-300 focus:bg-neutral-100 dark:focus:bg-neutral-900 w-[400px] rounded-md mt-3 px-3 py-2 focus:outline-none border-[1px] dark:border-neutral-800 focus:border-[1px] focus:border-neutral-300" placeholder={placeholder} />
+    </div>
 }
 
 
