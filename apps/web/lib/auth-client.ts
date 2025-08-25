e1604075a882debe57d7e26692f0cb62834608f1
Akdev Saha <akdevsaha2003@gmail.com>
2025-08-25 17:32:21 +0530

    feat: configure Better-Auth client with type inference

--- apps/web/lib/auth-client.ts
+++ apps/web/lib/auth-client.ts
@@ -1,6 +1,10 @@
-import { createAuthClient } from "better-auth/react"
+import { inferAdditionalFields } from "better-auth/client/plugins";
+import { auth } from "../../api/src/auth";
+import { createAuthClient } from "better-auth/react";
 
-export const authClient: ReturnType<typeof createAuthClient> = createAuthClient({
-    baseURL: "http://localhost:3000"
-})
+export const authClient = createAuthClient({
+  baseURL: "http://localhost:3000",
+  plugins: [inferAdditionalFields<typeof auth>()],
+});
 
+export type Session = typeof authClient.$Infer.Session;
